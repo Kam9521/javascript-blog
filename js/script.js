7e0a2fe8a0ebc08e37b07954bdee1945a744171e
Kamil <szpmytyl@wp.pl>
2026-04-02 10:25:56 +0200

    Implement article switching on title click

---
+++ js/script.js
@@ -0,0 +1,50 @@
+"use strict";
+{
+  const titleClickHandler = function (event) {
+    event.preventDefault();
+    const clickedElement = this;
+
+    console.log("Link was clicked!");
+    console.log("event:", event);
+
+    /* [DONE] remove class 'active' from all article links */
+
+    const activeLinks = document.querySelectorAll(".titles a.active");
+
+    for (let activeLink of activeLinks) {
+      activeLink.classList.remove("active");
+    }
+
+    /* [DONE] add class 'active' to the clicked link */
+
+    clickedElement.classList.add("active");
+
+    /* [DONE] remove class 'active' from all articles */
+
+    const activeArticles = document.querySelectorAll(".posts .post.active");
+
+    for (let activeArticle of activeArticles) {
+      activeArticle.classList.remove("active");
+    }
+
+    /* [DONE] get 'href' attribute from the clicked link */
+
+    const articleSelector = clickedElement.getAttribute("href");
+    console.log("articleSelector:", articleSelector);
+
+    /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+
+    const targetArticle = document.querySelector(articleSelector);
+    console.log("targetArticle:", targetArticle);
+
+    /* [DONE] add class 'active' to the correct article */
+
+    targetArticle.classList.add("active");
+  };
+
+  const links = document.querySelectorAll(".titles a");
+
+  for (let link of links) {
+    link.addEventListener("click", titleClickHandler);
+  }
+}
